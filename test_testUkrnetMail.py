92be4334451138a0d071d3d563043b2a200d3916
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-07-22 10:48:25 +0300

    It's WORKED

--- test_testUkrnetMail.py
+++ test_testUkrnetMail.py
@@ -14,7 +14,11 @@ class LoginMailBox(unittest.TestCase):
         login_field.send_keys("romareverse9")
         password_field = driver.find_element_by_id("id-p")
         password_field.send_keys("228228228ok")
-        
+        button_login = driver.find_element_by_css_selector(
+            'div.root:nth-child(1) div.app main.app__main form.form > button.button.button_style-main.button_size-regular.form__submit:nth-child(4)')
+        button_login.click()
+        user_mail = driver.find_element_by_xpath("//*[@class='login-button__user']")
+        assert user_mail.text == "romareverse9@ukr.net"
 
         if __name__ == "__main__":
             unittest.main()
