60863b919fc63dbabe35e24d1f81de2388177ac7
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-07-22 11:53:03 +0300

    Update1.1

--- test_testUkrnetMail.py
+++ test_testUkrnetMail.py
@@ -2,23 +2,42 @@ import unittest
 from selenium import webdriver
 
 
+# импорт библиотек
+
 class LoginMailBox(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path='C:\Drivers\chromedriver.exe')
         self.driver.implicitly_wait(10)
 
+    # создаём класс, указываем путь к драйверу и выставляем timeout
+
     def test_testUkrnetMail(self):
         driver = self.driver
         driver.get("https://mail.ukr.net/desktop/login")
+        self.driver.maximize_window()
+        self.login(driver, login="romareverse9", password="228228228ok")
+        self.validate_login(driver)
+        drop_bar = driver.find_element_by_css_selector(
+            "body.theme-white:nth-child(2) div.contacts-shown:nth-child(1) header.header.animate div.header__login div.login div.login__login-button a.login-button > p.login-button__user")
+        drop_bar.click()
+        log_out = driver.find_element_by_id("login__logout")
+        log_out.click()
+
+    def validate_login(self, driver):
+        user_mail = driver.find_element_by_xpath("//*[@class='login-button__user']")
+        assert user_mail.text == "romareverse9@ukr.net"
+
+    def login(self, driver, login, password):
         login_field = driver.find_element_by_id("id-l")
-        login_field.send_keys("romareverse9")
+        login_field.send_keys(login)
         password_field = driver.find_element_by_id("id-p")
-        password_field.send_keys("228228228ok")
+        password_field.send_keys(password)
         button_login = driver.find_element_by_css_selector(
             'div.root:nth-child(1) div.app main.app__main form.form > button.button.button_style-main.button_size-regular.form__submit:nth-child(4)')
         button_login.click()
-        user_mail = driver.find_element_by_xpath("//*[@class='login-button__user']")
-        assert user_mail.text == "romareverse9@ukr.net"
+
+    def tearDown(self):
+        self.driver.quit()
 
         if __name__ == "__main__":
             unittest.main()
