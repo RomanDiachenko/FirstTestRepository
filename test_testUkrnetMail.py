5266e494f7939032eb00b64f63ec74f8eee78c38
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-07-23 16:28:12 +0300

    get code structure

--- test_testUkrnetMail.py
+++ test_testUkrnetMail.py
@@ -14,13 +14,14 @@ class LoginMailBox(unittest.TestCase):
         driver = self.driver
         driver.get("https://mail.ukr.net/desktop/login")
         self.driver.maximize_window()
-        self.login(driver, login="romareverse9", password="228228228ok")
-        self.mail_send(driver)
-        self.tab_test(driver)
-        self.search_mail(driver)
-        self.log_out_mail(driver)
+        self.login(login="romareverse9", password="228228228ok")
+        self.mail_send()
+        self.tab_test()
+        self.search_mail()
+        self.log_out_mail()
 
-    def search_mail(self, driver):
+    def search_mail(self):
+        driver = self.driver
         driver.find_element_by_xpath("//input[@placeholder='Пошук']").send_keys("test")
         ActionChains(driver).send_keys(Keys.ENTER)
         time.sleep(3)
@@ -29,7 +30,8 @@ class LoginMailBox(unittest.TestCase):
         driver.find_element_by_xpath("//a[@class='controls-link remove']").click()
         time.sleep(1)
 
-    def tab_test(self, driver):
+    def tab_test(self):
+        driver = self.driver
         driver.find_element_by_xpath("//a[@id='0']//span[@class='sidebar__list-link-name']").click()
         time.sleep(0.5)
         driver.find_element_by_xpath("//a[@id='10002']//span[@class='sidebar__list-link-name']").click()
@@ -48,11 +50,14 @@ class LoginMailBox(unittest.TestCase):
             "//a[@class='sidebar__list-link files']//span[@class='sidebar__list-link-name']").click()
         time.sleep(1)
 
-    def log_out_mail(self, driver):
+    def log_out_mail(self):
+        driver = self.driver
         driver.find_element_by_xpath("//a[@class='login-button']").click()
+        time.sleep(1)
         driver.find_element_by_id("login__logout").click()
 
-    def login(self, driver, login, password):
+    def login(self, login, password):
+        driver = self.driver
         driver.find_element_by_id("id-l").send_keys(login)
         driver.find_element_by_id("id-p").send_keys(password)
         driver.find_element_by_xpath(
@@ -60,7 +65,8 @@ class LoginMailBox(unittest.TestCase):
         user_mail = driver.find_element_by_xpath("//*[@class='login-button__user']")
         assert user_mail.text == "romareverse9@ukr.net"
 
-    def mail_send(self, driver):
+    def mail_send(self):
+        driver = self.driver
         driver.find_element_by_xpath("//button[@class='default compose']").click()
         driver.find_element_by_xpath("//input[@name='toFieldInput']").send_keys("romareverse9@gmail.com")
         driver.find_element_by_xpath("//input[@name='subject']").send_keys("Some text")
