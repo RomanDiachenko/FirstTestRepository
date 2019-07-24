9980d2c1673ab3131ab350817600f0f76963d398
RomanDiachenko <52920901+RomanDiachenko@users.noreply.github.com>
2019-07-24 09:39:36 +0300

    sett

--- application.py
+++ application.py
@@ -1,74 +1,34 @@
-import time
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
+
+def __init__(self, email):
+    self.email = email
 
 
-class Application:
+def test_testUkrnetMail(self):
+    driver = self.driver
+    driver.get("https://mail.ukr.net/desktop/login")
+    self.driver.maximize_window()
 
-    def __init__(self):
-        self.driver = webdriver.Chrome(executable_path='C:\Drivers\chromedriver.exe')
-        self.driver.implicitly_wait(10)
+    self.login(login="romareverse9", password="228228228ok")
+    self.mail_send()
+    self.tab_test()
+    self.search_mail()
+    self.log_out_mail()
 
-    def open_page(self):
-        driver = self.driver
-        driver.get("https://mail.ukr.net/desktop/login")
 
-    def login(self, login, password):
-        driver = self.driver
-        self.open_page()
-        driver.find_element_by_id("id-l").send_keys(login)
-        driver.find_element_by_id("id-p").send_keys(password)
-        driver.find_element_by_xpath(
-            "//button[@class='button button_style-main button_size-regular form__submit']").click()
-        user_mail = driver.find_element_by_xpath("//*[@class='login-button__user']")
-        assert user_mail.text == "romareverse9@ukr.net"
+class Person:
 
-    def log_out_mail(self):
-        driver = self.driver
-        driver.find_element_by_xpath("//a[@class='login-button']").click()
-        self.driver.maximize_window()
-        time.sleep(1)
-        driver.find_element_by_id("login__logout").click()
+    # конструктор
+    def __init__(self, name):
+        self.name = name  # устанавливаем имя
 
-    def search_mail(self):
-        driver = self.driver
-        driver.find_element_by_xpath("//input[@placeholder='Пошук']").send_keys("test")
-        ActionChains(driver).send_keys(Keys.ENTER)
-        time.sleep(3)
-        driver.find_element_by_xpath("//a[1]//div[1]//span[1]").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//a[@class='controls-link remove']").click()
-        time.sleep(1)
+    def display_info(self):
+        print("Привет, меня зовут", self.name)
 
-    def tab_test(self):
-        driver = self.driver
-        driver.find_element_by_xpath("//a[@id='0']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='10002']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='10001']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='10003']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='10004']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='unread']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath("//a[@id='marked']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath(
-            "//a[@class='sidebar__list-link files']//span[@class='sidebar__list-link-name']").click()
-        time.sleep(1)
 
-    def mail_send(self):
-        driver = self.driver
-        driver.find_element_by_xpath("//button[@class='default compose']").click()
-        driver.find_element_by_xpath("//input[@name='toFieldInput']").send_keys("romareverse9@gmail.com")
-        driver.find_element_by_xpath("//input[@name='subject']").send_keys("Some text")
-        driver.execute_script("tinyMCE.activeEditor.setContent('%s')" % "some text from this test case")
-        driver.find_element_by_xpath("//button[@class='default send']").click()
-        time.sleep(2)
+person1 = Person("Tom")
+person1.display_info()  # Привет, меня зовут Tom
+person2 = Person("Sam")
+person2.display_info()  # Привет, меня зовут Sam
 
-    def destroy(self):
-        self.driver.quit()
+
+person1 = emai
